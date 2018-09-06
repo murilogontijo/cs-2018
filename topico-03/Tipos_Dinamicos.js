dcf5887cce3bfd150ee69647b783172bd94fe25a
Murilo Gontijo <murilob13@gmail.com>
2018-09-05 21:40:55 -0300

    Topico 03 - Experimentos de execução usando o Node.js

---
+++ topico-03/Tipos_Dinamicos.js
@@ -0,0 +1,6 @@
+// O tipo de uma vari�vel em JavaScript pode ser alterado durante a execu��o
+let x = "Mensagem";
+console.log("x � do tipo " + (typeof x));
+
+x = 9.8;
+console.log("x � do tipo " + (typeof x));
