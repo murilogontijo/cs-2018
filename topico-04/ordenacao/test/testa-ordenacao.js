e77520f00f3f55bb66f0792fc0992d2500f01fda
Murilo Gontijo <murilob13@gmail.com>
2018-10-03 17:25:16 -0300

    Correção de Testes

--- topico-04/ordenacao/test/testa-ordenacao.js
+++ topico-04/ordenacao/test/testa-ordenacao.js
@@ -1,23 +1,3 @@
-// Obtém referência para o módulo que contém função a ser testada.
-// Observe que o presente código de teste está
-// em diretório específico (test), enquanto o
-// código testado está isolado em outro diretório.
-
-// const operacao = require("../codigo");
-
-// Abaixo segue o nosso primeiro teste
-// identificado por "soma trivial".
-
-// QUnit.test("soma trivial", function (assert) {
-
-  // Executa a operação que desejamos testar
-  // let resultado = operacao.soma(3, -1);
-
-  // Verifica se o resultado produzido é o esperado.
-  // assert.equal(resultado, 2, "soma incorreta");
-// });
-
-
 const ordenacao = require("../ordenacao");
 
 QUnit.test("Ordenar frase a frase: A vila e bela", function(assert){
