60d82f81e242090aebb81ce8014567a7c2540aa3
Murilo Gontijo <murilob13@gmail.com>
2018-10-11 15:35:11 -0300

    Praticando exercícios

---
+++ topico-04/ambientacao/es6/lib/soma.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _matematica = require("./matematica");
+
+var _matematica2 = _interopRequireDefault(_matematica);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var funcoes = new _matematica2.default(); // ES6
+
+console.log(funcoes.soma(1, 2) + funcoes.subtracao(1, 2));
